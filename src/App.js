afba50525a93fb32e9118f56cbf5f042259a6fdf
Esma Engin <esmaengin91@gmail.com>
2023-01-10 18:40:33 +0300

    olmadı ama olmuş gibi yapalım..

--- src/App.js
+++ src/App.js
@@ -1,12 +1,67 @@
 import React from 'react';
-import { CssBaseLine, Container, Grid, AppBar, ToolBar, Typography, Button, IconButton } from "material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  CssBaseline, 
+  Container, 
+  Grid, 
+  AppBar, 
+  Toolbar, 
+  Typography, 
+  Button, 
+  IconButton,
+ } from "@material-ui/core";
+
+import { BrowserRouter as Router, Routes, Route, redirect } from "react-router-dom";
+import PenIcon from "@material-ui/icons/Create";
+import PostsList from "./components/PostsList";
+
+const useStyles = makeStyles( theme => ({
+  root: {
+   flexGrow: 1, //genişleyebileceği kadar genişlemesi için
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  container: {
+    marginTop: theme.spacing(3),
+  },
+}));
 
 const App = () => {
+
+  const classes = useStyles();
   return (
     <>
-    <CssBaseLine/>
-    </>
-  )
-}
+    <CssBaseline/>
+    <Container maxWidth="lg">
+        <AppBar position="static" color= "inherit" elevation={0}>
+          <Toolbar>
+            <IconButton edge="start" className={classes.container} color="inherit"/>
+            <Typography variant="h6" color="secondary" className={classes.title}>
+                <a href="http://localhost:3000/posts">BiBlog</a>
+            </Typography>
 
+            <Button color="primary" variant="outlined" startIcon={<PenIcon/>}>
+              Yeni Yazı
+            </Button>
+          </Toolbar>
+        </AppBar>
+        <Grid container className={classes.container}>
+          <Grid item xs={12}/*en küçük ekranda bile bütün ekranı kaplayacak*/> 
+            <Router>
+              <Routes>
+                <Route exact path="/posts" component={PostsList}/>
+              </Routes>
+
+              <redirect from="/" to="/posts" />
+            </Router>
+          </Grid>
+        </Grid>
+    </Container>  
+    </>
+  );
+};
 export default App;
